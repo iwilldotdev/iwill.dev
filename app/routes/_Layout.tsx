4296a582b6f68470bc7d8d6db4bbf6c70c57783a
William Gonçalves <euwilliamgoncalves@gmail.com>
2025-04-29 08:41:55 -0300

    feat: layout route

---
+++ app/routes/_Layout.tsx
@@ -0,0 +1,56 @@
+import { Link, Outlet, type LoaderFunctionArgs } from "react-router";
+import { cn } from "~/lib/utils";
+import type { Route } from "./+types/_Layout";
+
+export function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const pathname = url.pathname;
+  return [
+    {
+      label: "Home",
+      path: "/",
+      active: pathname === "/",
+    },
+    {
+      label: "Trabalho",
+      path: "/path",
+      active: pathname === "/path",
+    },
+    {
+      label: "Feed",
+      path: "/feed",
+      active: pathname === "/feed",
+    },
+  ];
+}
+
+export default function Layout({ loaderData }: Route.ComponentProps) {
+  return (
+    <>
+      <header className="fixed bottom-0 z-50 flex h-12 w-full items-center justify-center bg-neutral-900 text-sm lg:top-0 lg:bottom-auto lg:text-base">
+        <nav>
+          <ul className="flex list-none gap-4">
+            {loaderData.map((item, index) => (
+              <li key={index} className="grid place-items-center px-2 lg:w-28">
+                <Link
+                  className={cn(
+                    "font-light no-underline",
+                    item.active && "cursor-default",
+                    !item.active &&
+                      "opacity-50 hover:font-normal hover:underline",
+                  )}
+                  to={item.path}
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <main className="size-full">
+        <Outlet />
+      </main>
+    </>
+  );
+}
