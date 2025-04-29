a379b002d79e1eedebc278e774d82dc2456a380d
William Gonçalves <euwilliamgoncalves@gmail.com>
2025-04-29 08:40:13 -0300

    fix: container component composition

--- app/components/layout/container.tsx
+++ app/components/layout/container.tsx
@@ -1,16 +1,27 @@
+import type { ElementType } from "react";
+import { cn } from "~/lib/utils";
+
 export function Container({
   children,
+  className,
   id,
+  element = "main",
 }: {
   children: React.ReactNode;
+  className?: string;
   id?: string;
+  element?: ElementType;
 }) {
+  const Comp = element;
   return (
-    <main
+    <Comp
       id={id}
-      className="relative flex min-h-full w-full flex-col gap-4 overflow-x-hidden p-8 pt-8 text-white lg:p-16 lg:pt-32"
+      className={cn(
+        "relative flex min-h-full w-full flex-col gap-4 overflow-x-hidden p-8 pt-8 text-white lg:p-32 lg:pt-32",
+        className,
+      )}
     >
       {children}
-    </main>
+    </Comp>
   );
 }
