a3eb4ca3b7bdffaccb73b195b881b565804e3042
William Gonçalves <euwilliamgoncalves@gmail.com>
2025-06-21 14:53:23 -0300

    fix: sitemap content

--- app/routes/sitemap[.xml].tsx
+++ app/routes/sitemap[.xml].tsx
@@ -1,32 +1,31 @@
 import { getPosts } from "~/lib/posts.server.js";
 
 const DOMAIN_URL = "https://iwill.dev";
+const currentDate = new Date().toISOString();
 
 export async function loader() {
   const posts = getPosts();
 
+  const staticPages = [
+    { path: "/", priority: "1.0" },
+    { path: "/feed", priority: "0.8" },
+    { path: "/path", priority: "0.8" },
+    { path: "/links", priority: "0.8" },
+  ];
+
   const sitemapXml = `
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-      <url>
-        <loc>https://www.iwill.dev/</loc>
-        <lastmod>2025-06-21T16:50:01+00:00</lastmod>
-        <priority>1.00</priority>
-      </url>
-      <url>
-        <loc>https://www.iwill.dev/path</loc>
-        <lastmod>2025-06-21T16:50:01+00:00</lastmod>
-        <priority>0.80</priority>
-      </url>
-      <url>
-        <loc>https://www.iwill.dev/feed</loc>
-        <lastmod>2025-06-21T16:50:01+00:00</lastmod>
-        <priority>0.80</priority>
-      </url>
-      <url>
-        <loc>https://www.iwill.dev/links</loc>
-        <lastmod>2025-06-21T16:50:01+00:00</lastmod>
-        <priority>0.80</priority>
-      </url>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${staticPages
+        .map(
+          (page) => `
+        <url>
+          <loc>${DOMAIN_URL}${page.path}</loc>
+          <lastmod>${currentDate}</lastmod>
+          <priority>${page.priority}</priority>
+        </url>
+      `,
+        )
+        .join("")}
       ${posts
         .map(
           (post) => `
