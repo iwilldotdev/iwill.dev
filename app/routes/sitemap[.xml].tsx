6b25d5193ba58c0911eceac9d39db1a2bb067676
William Gonçalves <euwilliamgoncalves@gmail.com>
2025-06-21 16:23:29 -0300

    fix: update sitemap.xml

--- app/routes/sitemap[.xml].tsx
+++ app/routes/sitemap[.xml].tsx
@@ -14,6 +14,7 @@ export async function loader() {
   ];
 
   const sitemapXml = `
+    <?xml version="1.0" encoding="UTF-8"?> 
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${staticPages
         .map(
